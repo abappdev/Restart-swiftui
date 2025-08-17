a262cf7fcc02a997fb777317b560a6f3190eea2d
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2025-08-17 20:55:43 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Abhishek Bhalerao on 17/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
