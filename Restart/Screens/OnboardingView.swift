583af0effc8a371bdd53c3ee52aee5d881ae7741
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2025-08-22 19:43:48 +0530

    Redesign onboarding screen UI
    Revamps the OnboardingView with a new layout, background color, updated typography, and custom graphics. Adds a motivational quote, a character image with decorative circles, and a redesigned 'Get Started' button for improved user experience.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,35 +12,95 @@ struct OnboardingView: View {
     @AppStorage("isOnboarding") var isOnboardingViewActive: Bool = false
 
     var body: some View {
-        VStack {
-            VStack {
-                Image(systemName: "greetingcard.fill")
-                    .imageScale(.large)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
 
-                Text("Onboarding!")
-            }
-            .foregroundStyle(
-                LinearGradient(
-                    colors: [Color.colorRed, Color.colorBlue],
-                    startPoint: .bottomTrailing,
-                    endPoint: .topLeading
-                )
-            )
+            VStack(spacing: 20) {
+                Spacer()
 
-            Button(
-                action: {
-                    
-                    isOnboardingViewActive.toggle()
+                VStack(spacing: 0) {
 
-                    
-                },
-                label: {
-                    Text("Continue")
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+
+                    Text(
+                        """
+                        Love blossoms when we freely share,
+                        For in giving, hearts truly care.
+                        """
+                    )
+                    .font(.system(size: 20))
+                    .fontWeight(.light)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
                 }
-            )
 
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 300, height: 300, alignment: .center)
+
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 300, height: 300, alignment: .center)
+                    }
+
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+
+                Spacer()
+
+                ZStack {
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .offset(x: 20)
+
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80, height: 80)
+
+                        Spacer()
+                    }
+
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+
+                        }.foregroundStyle(.white)
+                            .frame(width: 80, height: 80, alignment: .center)
+
+                        Spacer()
+                    }
+
+                }
+                .frame(height: 80, alignment: .center)
+                .padding(20)
+            }
         }
-        .padding()
     }
 }
 
